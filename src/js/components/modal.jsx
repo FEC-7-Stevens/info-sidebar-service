90be0c24ac101aea8126d25be94b852af7b17356
Josh Rogen <rogenjh@gmail.com>
2020-08-22 17:53:25 -0400

    z-index attempt

--- src/js/components/modal.jsx
+++ src/js/components/modal.jsx
@@ -80,10 +80,6 @@ const Button = styled.button`
   }
 `;
 
-const MapFrame = styled.section`
-  z-index: 600;
-`;
-
 function Modal({
   handleClick, restaurantName, streetAddress, city, state, zipCode, clicked, open
 }) {
@@ -103,18 +99,17 @@ function Modal({
           <Button>
             X
           </Button>
-          <MapFrame>
-            <iframe
-              title="restaurantModal"
-              width="100%"
-              height="175%"
-              cookieFlags="samesite=none;secure"
-              frameBorder="0"
-              src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyDPIUHkZ4FNCluRm_Vbm-Hp3pjqg42-Anw
-              &q=${queryString}`}
-              allowFullScreen
-            />
-          </MapFrame>
+          <iframe
+            title="restaurantModal"
+            width="100%"
+            height="175%"
+            cookieFlags="samesite=none;secure"
+            frameBorder="0"
+            style={{ 'z-index': '601' }}
+            src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyDPIUHkZ4FNCluRm_Vbm-Hp3pjqg42-Anw
+            &q=${queryString}?wmode=opaque`}
+            allowFullScreen
+          />
         </Backdrop>
       )}
     </div>
